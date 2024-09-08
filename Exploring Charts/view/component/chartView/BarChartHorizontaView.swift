0ff8c17610c43a806104539c3be00684cf622d94
uwi0 <kakapoweb@gmail.com>
2024-09-09 04:28:30 +0700

    add button in demo 3 and create bar chart, to separate chart view from demo 3 to specific file

---
+++ Exploring Charts/view/component/chartView/BarChartHorizontaView.swift
@@ -0,0 +1,18 @@
+//
+//  BarChartHorizontaView.swift
+//  Exploring Charts
+//
+//  Created by dwi prasetyo on 09/09/24.
+//
+
+import SwiftUI
+
+struct BarChartHorizontaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarChartHorizontaView()
+}
---
+++ Exploring Charts/view/component/chartView/BarChartVerticalView.swift
@@ -0,0 +1,18 @@
+//
+//  BarChartVerticalView.swift
+//  Exploring Charts
+//
+//  Created by dwi prasetyo on 09/09/24.
+//
+
+import SwiftUI
+
+struct BarChartVerticalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarChartVerticalView()
+}
