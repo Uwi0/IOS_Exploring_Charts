0e9051b3ebd5d67dc2cf7a4ffc7c9a33bbe85d16
uwi0 <kakapoweb@gmail.com>
2024-09-08 10:44:37 +0700

    Initial Commit

---
+++ Exploring Charts/Exploring_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exploring_ChartsApp.swift
+//  Exploring Charts
+//
+//  Created by dwi prasetyo on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Exploring_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
