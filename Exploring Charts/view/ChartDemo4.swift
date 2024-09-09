1a1229d187d14843c70b07e47a7a661f9aeb2912
uwi0 <kakapoweb@gmail.com>
2024-09-10 05:06:43 +0700

    add implementation edit mode in ChartDemo4

--- Exploring Charts/view/ChartDemo4.swift
+++ Exploring Charts/view/ChartDemo4.swift
@@ -3,59 +3,87 @@ import Charts
 
 struct ChartDemo4: View {
     let dailySales: [DailySalesType]
-    let min: Double
-    let max: Double
-    let xAxisMarkPosition: AxisMarkPosition = .bottom
-    let yAxisMarkPosition: AxisMarkPosition = .leading
     @State private var chartType: ChartType = .bar
     @State private var isVerticalChart = true
     @State private var barColors: [Color] = defaultBarColors
     @State var title = "Chart Title"
     @State var titleAlignment: HorizontalAlignment = .trailing
+    @State var isEditMode: Bool = false
+    
+    private var editModeIcon: String {
+        isEditMode ? "checkmark" : "square.and.pencil"
+    }
     
     var body: some View {
-        HStack {
-            
-            LeftChartButtonView(
-                chartType: $chartType,
-                barColors: $barColors,
-                isVerticalMode: $isVerticalChart
-            )
-            
-            VStack(alignment: titleAlignment) {
-                Text(title)
-                    .font(.headline)
-                    .fontWeight(.semibold)
-                if isVerticalChart {
-                    switch(chartType) {
-                    case .area: AreaChartVerticalView(dailySales: dailySales)
-                    case .bar: BarChartVerticalView(dailySales: dailySales, barColors: barColors)
-                    case .line: LineChartVerticalView(dailySales: dailySales)
+        VStack {
+            HStack {
+                Button(
+                    action: {
+                        withAnimation {
+                            isEditMode.toggle()
+                        }
+                    },
+                    label: {
+                        Image(systemName: editModeIcon)
                     }
-                } else {
-                    switch(chartType) {
-                    case .area: AreaChartHorizontaView(dailySales: dailySales)
-                    case .bar: BarChartHorizontalView(dailySales: dailySales, barColors: barColors)
-                    case .line: LineChartHorizontaView(dailySales: dailySales)
-                    }
+                )
+                if !isEditMode {
+                    Button(
+                        action: {
+                            withAnimation {
+                                //TODO: Sharing
+                            }
+                        },
+                        label: {
+                            Image(systemName: "square.and.arrow.up")
+                        }
+                    )
                 }
             }
-            
-            
-            
-            RightChartButtonView(
-                isVerticalMode: $isVerticalChart,
-                titleAlignment: $titleAlignment
-            )
-        }.padding()
+            HStack {
+                
+                if isEditMode {
+                    LeftChartButtonView(
+                        chartType: $chartType,
+                        barColors: $barColors,
+                        isVerticalMode: $isVerticalChart
+                    )
+                }
+                
+                VStack(alignment: titleAlignment) {
+                    Text(title)
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                    if isVerticalChart {
+                        switch(chartType) {
+                        case .area: AreaChartVerticalView(dailySales: dailySales)
+                        case .bar: BarChartVerticalView(dailySales: dailySales, barColors: barColors)
+                        case .line: LineChartVerticalView(dailySales: dailySales)
+                        }
+                    } else {
+                        switch(chartType) {
+                        case .area: AreaChartHorizontaView(dailySales: dailySales)
+                        case .bar: BarChartHorizontalView(dailySales: dailySales, barColors: barColors)
+                        case .line: LineChartHorizontaView(dailySales: dailySales)
+                        }
+                    }
+                }
+                
+                if isEditMode {
+                    RightChartButtonView(
+                        isVerticalMode: $isVerticalChart,
+                        titleAlignment: $titleAlignment
+                    )
+                }
+                
+            }.padding()
+        }
+        .padding()
         
     }
     
 }
 
 #Preview {
-    ChartDemo4(
-        dailySales: defaultDailySales,
-        min: 0.0,
-        max: 700.0)
+    ChartDemo4(dailySales: defaultDailySales)
 }
