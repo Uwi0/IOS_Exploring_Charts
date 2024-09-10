8bb4005da2a8b0d771db0f234bfc8771047cc87f
uwi0 <kakapoweb@gmail.com>
2024-09-10 07:17:09 +0700

    move some file to specific folder and add rule marker

---
+++ Exploring Charts/view/component/chartView/component/RuleMarkView.swift
@@ -0,0 +1,18 @@
+//
+//  RuleMarkView.swift
+//  Exploring Charts
+//
+//  Created by dwi prasetyo on 10/09/24.
+//
+
+import SwiftUI
+
+struct RuleMarkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RuleMarkView()
+}
