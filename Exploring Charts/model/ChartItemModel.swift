118637ddce97cf9c484361dca3dabd04dc8a8d0e
uwi0 <kakapoweb@gmail.com>
2024-09-11 19:03:15 +0700

    creating chart item for onganizing chart 4 demo property

--- Exploring Charts/model/ChartItemModel.swift
+++ Exploring Charts/model/ChartItemModel.swift
@@ -1,5 +1,13 @@
-import Foundation
+import SwiftUI
 
 struct ChartItemModel: Identifiable {
-    
+    let id = UUID()
+    var dailySales: [DailySalesType]
+    var barColor: [Color]
+    var editMode: Bool
+    var selectedDay: String
+    var title: String
+    var titleAlignment: HorizontalAlignment
+    var charType: ChartType
+    var isVerticalChart: Bool
 }
